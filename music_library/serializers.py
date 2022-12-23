7cd6c25df6dcfa38ae38f3ce5a727535f26f6456
Nicholas <zantladjevich@gmail.com>
2022-12-22 22:17:15 -0500

    Created POST request

--- music_library/serializers.py
+++ music_library/serializers.py
@@ -4,4 +4,4 @@ from .models import Songs
 class SongsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Songs
-        fields = ['title', 'artist', 'album', 'release_date', 'genre']
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
--- music_library/views.py
+++ music_library/views.py
@@ -2,16 +2,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Songs
 from .serializers import SongsSerializer
+from rest_framework import status
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-    songs = Songs.objects.all()
-
-    serializer = SongsSerializer(songs, many=True)
-
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        songs = Songs.objects.all()
+        serializer = SongsSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongsSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
