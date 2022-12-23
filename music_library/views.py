f72710340dbcad6b9ff93df86462b8f71085ea09
Nicholas <zantladjevich@gmail.com>
2022-12-22 20:48:42 -0500

    Updated get request function

--- music_library/views.py
+++ music_library/views.py
@@ -1,13 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Songs
+from .serializers import SongsSerializer
 
 # Create your views here.
 
 @api_view(['GET'])
 def cars_list(request):
+    songs = Songs.objects.all()
 
+    serializer = SongsSerializer(songs, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
 
 
 
